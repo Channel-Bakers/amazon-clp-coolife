2a2ab5ba27679c6e243ce4e5bf2b4b1eed7449cf
Ryan DiMascio <ryand@channelbakers.com>
2020-01-30 12:05:37 -0800

    rename softside -> softSide

--- src/js/routes/routes.js
+++ src/js/routes/routes.js
@@ -4,12 +4,12 @@ import common from './common';
 import onePiece from './onePiece';
 import luggageSet from './luggageSet';
 import business from './business';
-import softside from './softside';
+import softSide from './softSide';
 
 export default {
 	common,
 	onePiece,
 	luggageSet,
 	business,
-	softside,
+	softSide,
 };
